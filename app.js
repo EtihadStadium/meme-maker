2dd9da378a28af877452ad37a551f3e042438d2b
EtihadStadium <mancity@mailbox.org>
2023-03-23 16:41:55 +0900

    Add mouse paint

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+let isPainting = false;
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
+canvas.addEventListener("mousemove", (event) => {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+});
+canvas.addEventListener("mousedown", () => {
+    isPainting = true;
+});
+canvas.addEventListener("mouseup", () => {
+    isPainting = false;
+});
