6e728d792c635b20c9c1201168c3ef1e2422e325
EtihadStadium <mancity@mailbox.org>
2023-04-02 17:51:17 +0900

    Add eraser

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const p = document.querySelector("p");
 const color = document.getElementById("color");
 const colorOption = Array.from(document.getElementsByClassName("color-option"));
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 let isPainting = false;
 let isFilling = false;
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 p.innerText = lineWidth.value;
 function cancelPainting() {
@@ -30,7 +34,7 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 canvas.addEventListener("click", () => {
     if (isFilling) {
-        ctx.fillRect(0, 0, 800, 800);
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 });
 lineWidth.addEventListener("change", (event) => {
@@ -57,4 +61,13 @@ modeBtn.addEventListener("click", () => {
         isFilling = true;
         modeBtn.innerText = "Draw";
     }
+});
+destroyBtn.addEventListener("click", () => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+});
+eraserBtn.addEventListener("click", () => {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
 });
